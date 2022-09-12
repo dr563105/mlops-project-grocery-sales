99e091b11be73eb901849ddfaaeb8074d6411a53
Ubuntu <ubuntu@ip-172-31-25-64.ec2.internal>
2022-09-12 00:44:25 +0000

    added timeout to request.post method

--- deployment/test_requests.py
+++ deployment/test_requests.py
@@ -3,5 +3,5 @@ import requests
 find = {"date1": "2022-09-15", "store_nbr": 19}
 
 url = "http://localhost:9696/predict-sales"
-response = requests.post(url, json=find)
+response = requests.post(url, json=find, timeout=5)
 print(response.json())
