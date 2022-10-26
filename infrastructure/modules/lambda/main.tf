97f9936f755770c305b278fbdf53aa1bcc431324
Deepak Ramani <dr563105@gmail.com>
2022-10-26 05:40:50 +0530

    buggy-adding lambda to terraform

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "forecast-sales-lambda" {
   # which would be anyway updated regularly via a CI/CD pipeline
   image_uri = var.image_uri   # required-argument
   package_type = "Image"
-#   role          = aws_iam_role.iam_lambda.arn
+  role          = aws_iam_role.iam_lambda.arn
   tracing_config {
     mode = "Active"
   }
